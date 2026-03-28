d28ff9325751ae65dfbf751f2b8a0c475e86fa9f
Kevin Laptop <gemiankevin@yahoo.com>
2026-03-28 17:16:29 -0400

    Having Connection issues that will be fixed later

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,5 +3,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { AuthProvider } from './authProvider.jsx';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <AuthProvider>
+        <App />
+    </AuthProvider>
+);
